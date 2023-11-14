4f8fb509b4215cf2c602fc83d8f31eb66f82c2d6
찬영 이 <zizonxm@naver.com>
2023-11-15 00:17:45 +0900

    queue test2

---
+++ 05.Queue_t2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _05.Queue_t2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] progresses = { 95, 90, 99, 99, 80, 99 };
+            int[] speeds = { 1, 1, 1, 1, 1, 1};
+            int[] array = Solution.solution(progresses, speeds);
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ 05.Queue_t2/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Queue_t2
+{
+  
+    public static class Solution
+    {
+        public static int[] solution(int[] progresses, int[] speeds)
+        {
+            Queue<int> queue = new Queue<int>();
+            List<int> list = new List<int>();   
+            for (int i = 0; i < progresses.Length; i++)
+            {
+                int day = 1;
+                while (progresses[i] + speeds[i] * day < 100)
+                    day++;
+                queue.Enqueue(day);
+            }
+            while (queue.Count > 0)
+            {
+                int sum = 0;
+                int prevDay = queue.Dequeue();
+                sum++;
+                while (queue.Count > 0)
+                {
+                    if (queue.Peek() <= prevDay)
+                    {
+                        queue.Dequeue();
+                        sum++;
+                    }
+                    else
+                        break;
+                }
+                list.Add(sum);
+            }
+            return list.ToArray();
+        }
+    }
+}
